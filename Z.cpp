482b6516a2b378e59f6f75f3e59e977ce825453a
teriyakki-jin <122519436+teriyakki-jin@users.noreply.github.com>
2023-07-25 14:58:19 +0900

    Add files via upload

---
+++ Z.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+#include <vector>
+using namespace std;
+
+int ans;
+
+void Z(int y, int x, int size) {
+		
+}
+int main() {
+	vector< vector<int>> v;
+	int N,r,c;
+	cin >> N >> r >> c;
+	if()
+
+}
