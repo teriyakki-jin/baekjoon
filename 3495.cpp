0d8de383588311530be5d99555df691a9b5c7f6a
teriyakki-jin <122519436+teriyakki-jin@users.noreply.github.com>
2023-08-21 14:56:11 +0900

    Add files via upload

---
+++ 3495.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int h, w;
+    cin >> h >> w;
+
+    vector<string> arr(h);
+    for (int i = 0; i < h; i++) {
+        cin >> arr[i];
+    }
+
+    int dot = 0, slash = 0;
+
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            if (arr[i][j] == '\\' || arr[i][j] == '/') {
+                slash++;
+            }
+
+            if (slash % 2 == 1) {
+                if (arr[i][j] == '.') {
+                    dot++;
+                }
+            }
+        }
+    }
+
+    cout << (slash / 2) + dot;
+
+    return 0;
+}
